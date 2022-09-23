4eb38a52c06e53d6ae2e6f7f018a4f8bdad32547
MatheusPatricio013 <99891939+MatheusPatricio013@users.noreply.github.com>
2022-09-23 17:21:55 -0300

    Update script-beehive.sql

--- script-beehive.sql
+++ script-beehive.sql
@@ -10,7 +10,7 @@ telefone_celular varchar(15) unique not null,
 email varchar(50) unique not null,
 senha varchar(25) not null,
 cep char(8) not null,
-lograduro varchar(45) not null,
+logradouro varchar(45) not null,
 estado varchar(30) not null,
 cidade varchar(30) not null
 );
