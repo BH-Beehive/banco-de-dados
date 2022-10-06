af595a38db4feb41d9503e4e3677c5fef9f2518c
Leonardo Mariano Soares <lmsoares1@latam.stefanini.com>
2022-10-06 00:02:02 -0300

    Script atualizado

--- script-beehive.sql
+++ script-beehive.sql
@@ -18,9 +18,8 @@ cidade varchar(30) not null
 create table usuario_suporte(
 id_usuario int primary key auto_increment,
 nome_suporte varchar(45) not null,
-email varchar(50) unique not null,
-senha varchar(50) not null,
 email_slack varchar(50) unique not null,
+senha varchar(50) not null,
 telefone varchar(15) unique,
 celular char(15) unique,
 cpf char(14) unique not null,
@@ -30,21 +29,24 @@ foreign key (fk_empresa) references empresa(id_empresa)
 
 create table maquina(
 id_maquina int primary key auto_increment,
-host_name varchar(30) not null,
-fk_empresa int,
+host_name varchar(30) unique not null,
+token_acesso varchar(100) unique not null,
+token_ativo boolean not null,
+fk_empresa int ,
 foreign key (fk_empresa) references empresa(id_empresa),
-setor char(45),
+setor varchar(45) not null,
 nivel_prioridade char(1),
 check (nivel_prioridade = 1 or nivel_prioridade = 2 or nivel_prioridade = 3)
 )auto_increment = 100;
 
 create table registro(
 id_registro int primary key auto_increment,
-data_registro timestamp not null,
+data_registro timestamp default current_timestamp not null,
 fk_maquina int , 
-foreign key (fk_maquina) references maquina(id_maquina),
-cpu decimal,
-ram decimal,
-hd decimal
-
+foreign key (fk_maquina) references maquina(id_maquina)
 );
+
+
+
+
+
